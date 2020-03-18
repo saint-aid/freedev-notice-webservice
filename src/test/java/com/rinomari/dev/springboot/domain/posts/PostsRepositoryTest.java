865b4d49b4de23500359d19ddb151df32c40716e
saint-aid <ysp622@gmail.com>
2020-03-18 19:39:08 +0900

    커밋

--- src/test/java/com/rinomari/dev/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/rinomari/dev/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.rinomari.dev.springboot.domain.posts;
 
+import javafx.geometry.Pos;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -44,4 +46,28 @@ public class PostsRepositoryTest {
 
     }
 
+    @Test
+    public void BaseTimeEntity_등록(){
+        //given
+        LocalDateTime now = LocalDateTime.of(2019,6,4,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build()
+        );
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>> createdate= " + posts.getCreatedDate() +", modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+
+
+    }
 }
